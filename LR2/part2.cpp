2c513c83c95a6d5b4550111d5c1f2b54b5fdc624
elizoorg <el1zoorg@yandex.ru>
2023-02-28 12:45:11 +0300

    Using pthread_join. Set sched policy and priority by variants.

--- LR2/part2.cpp
+++ LR2/part2.cpp
@@ -8,10 +8,11 @@ pthread_t thread_id1;
 pthread_t thread_id2;
 pthread_attr_t attr1;
 pthread_attr_t attr2;
+
 void *long_thread1(void *notused)
 {
     int n;
-    for (n = 0; n < 5; n++)
+    for (n = 0; n < 10; n++)
     {
         printf("I992 - 1st thread, TID %d - N povtora %d\n", thread_id1, n);
     }
@@ -20,7 +21,7 @@ void *long_thread1(void *notused)
 void *long_thread2(void *notused)
 {
     int m;
-    for (m = 0; m < 5; m++)
+    for (m = 0; m < 10; m++)
     {
         printf("Eliseev Nikita - 2nd thread, TID %d - N povtora %d\n", thread_id1, m);
     }
@@ -28,20 +29,23 @@ void *long_thread2(void *notused)
 
 int main()
 {
+    // Вариант 4. Приоритеты : 6 12 . Алгоритмы : RR RR
     printf("Prog threads PID %d \n", getpid());
     pthread_attr_init(&attr1);
     pthread_attr_setinheritsched(&attr1, PTHREAD_EXPLICIT_SCHED);
-    pthread_attr_setschedpolicy(&attr1, SCHED_FIFO);
-    attr1.param.sched_priority = 2;
+    pthread_attr_setschedpolicy(&attr1, SCHED_RR);
+    attr1.param.sched_priority = 6;
 
     pthread_attr_init(&attr2);
     pthread_attr_setinheritsched(&attr2, PTHREAD_EXPLICIT_SCHED);
-    pthread_attr_setschedpolicy(&attr2, SCHED_FIFO);
-    attr2.param.sched_priority = 15;
+    pthread_attr_setschedpolicy(&attr2, SCHED_RR);
+    attr2.param.sched_priority = 12;
 
     pthread_create(&thread_id1, &attr1, long_thread1, NULL);
     pthread_create(&thread_id2, &attr2, long_thread2, NULL);
-    //TODO: Don't use sleep func . Use pthread_join instead. 
-    sleep(20);
+
+
+    pthread_join(thread_id1,NULL);
+    pthread_join(thread_id2,NULL);
     return (1);
 }
